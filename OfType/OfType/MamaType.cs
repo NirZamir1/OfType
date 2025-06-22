cf62f91ae2cc17b3796abb82d3de4308a144998b
NirZamir1 <nirozamir@gmail.com>
2025-06-22 18:32:41 +0300

    completed

---
+++ OfType/OfType/MamaType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfType
+{
+    internal class MamaType : IOfType
+    {
+        public IEnumerable<T> GetOfType<T>(IEnumerable enumerable)
+        {
+            List<T> list = new List<T>();
+            foreach (var item in enumerable) {
+                if (item is T  t)
+                {
+                    list.Add(t);
+                }
+            }
+            return list;
+        }
+
+        public IEnumerable<TOutput> GetOfType<TSource, TOutput>(IEnumerable<TSource> enumerable)
+        {
+            List<TOutput> list = new List<TOutput>();
+            foreach (TSource item in enumerable)
+            {
+                if (item is TOutput output)
+                {
+                    list.Add(output);
+                }
+            }
+            return list;
+        }
+
+        public IEnumerable<TBase> OfBase<TBase, TDerived>(IEnumerable<TDerived> derivedItems) where TDerived : TBase
+        {
+            List<TBase> list = new List<TBase>();
+            foreach (var derivedItem in derivedItems)
+            {
+               if(derivedItem is TBase derivedDerived)
+                    list.Add(derivedItem);
+            }
+            return list;
+        }
+    }
+}
---
+++ OfType/OfType/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace OfType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MamaType mama = new MamaType();
+            IEnumerable<string> res = mama.GetOfType<object, string>(new object[] { "dasdsa", 4, "dsasdas" });
+            Console.WriteLine(res.Count());
+            foreach (string s in res)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
